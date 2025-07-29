eb9ea656613cea164c32defba77abe0b28520fb4
scadreau <82372231+scadreau@users.noreply.github.com>
2025-07-29 00:54:31 -0700

    Update case_export.py to remove S3 file type prefix and adjust folder prefix
    - Removed the file type prefix from the S3 key generation for CSV exports.
    - Updated the last modified timestamp in the file metadata.

--- endpoints/exports/case_export.py
+++ endpoints/exports/case_export.py
@@ -1,5 +1,5 @@
 # Created: 2025-07-28 19:48:18
-# Last Modified: 2025-07-29 00:51:44
+# Last Modified: 2025-07-29 00:54:09
 # Author: Scott Cadreau
 
 # endpoints/exports/case_export.py
@@ -212,9 +212,9 @@ def export_cases_csv(request: CaseExportRequest):
                 
                 # Upload to S3 with custom folder prefix
                 s3_key = generate_s3_key(
-                    file_type="exports_csv",
+                    file_type="",
                     filename=filename,
-                    folder_prefix="/private/exports/csv/"
+                    folder_prefix="private/exports/csv/"
                 )
                 
                 s3_result = upload_file_to_s3(
